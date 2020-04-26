46bcfeb4861d51755dd6b3cca9cadf7b53aac316
yusuke-ota <33338085+yusuke-ota@users.noreply.github.com>
2020-04-26 20:15:46 +0900

    2020/04/26使用

---
+++ Assets/Scripts/CanvasOnOff.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+public class CanvasOnOff : MonoBehaviour
+{
+    private Transform _button;
+    private Transform _image;
+    private GameObject _rawImage;
+    private void Start()
+    {
+        _button = gameObject.transform.Find("Button");
+        _image = gameObject.transform.Find("Image");
+        _rawImage = _image.Find("RawImage").gameObject;
+    }
+
+    public void PushButton()
+    {
+        if (_rawImage.activeSelf)
+        {
+            _rawImage.SetActive(false);
+            _image.localScale = new Vector3(0.1f,0.1f,1f);
+            _image.localPosition = _button.localPosition;
+        }
+        else
+        {
+            _rawImage.SetActive(true);
+            _image.localScale = Vector3.one;
+            _image.localPosition = Vector3.zero;
+        }
+    }
+}
---
+++ Assets/Scripts/TrackingAction.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+public class TrackingAction : MonoBehaviour
+{
+    public TrackableObserver Observer;
+    
+    private void Start()
+    {
+#if !UNITY_EDITOR
+        Destroy(GameObject.Find("EmulatorRoom"));
+#endif
+        Observer.FoundEvent += FoundEvent;
+        Observer.LostEvnet += LostEvent;
+    }
+
+    private void FoundEvent(Vector3 position, Quaternion rotation)
+    {
+        
+    }
+
+    private void LostEvent()
+    {
+    }
+}
